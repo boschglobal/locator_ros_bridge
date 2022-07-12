0f5c54af45b7afa669f8fb6f582a343227385245
Stefan Laible <stefan.laible@de.bosch.com>
2022-07-12 13:41:28 +0200

    Use enum class instead of plain enum
    Signed-off-by: Stefan Laible <stefan.laible@de.bosch.com>

--- bosch_locator_bridge/src/locator_bridge_node.cpp
+++ bosch_locator_bridge/src/locator_bridge_node.cpp
@@ -331,7 +331,7 @@ void LocatorBridgeNode::laser_callback(const sensor_msgs::msg::LaserScan::Shared
     ++scan_num_,
     shared_from_this());
   if (laser_sending_interface_->sendData(laserscan_datagram.begin(), laserscan_datagram.size()) ==
-    SendingInterface::IO_EXCEPTION)
+    SendingInterface::SendingStatus::IO_EXCEPTION)
   {
     checkLaserScan(msg, "laser");
   }
@@ -353,7 +353,7 @@ void LocatorBridgeNode::laser2_callback(const sensor_msgs::msg::LaserScan::Share
     ++scan2_num_,
     shared_from_this());
   if (laser2_sending_interface_->sendData(laserscan_datagram.begin(), laserscan_datagram.size()) ==
-    SendingInterface::IO_EXCEPTION)
+    SendingInterface::SendingStatus::IO_EXCEPTION)
   {
     checkLaserScan(msg, "laser2");
   }
